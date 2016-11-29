f13f0d9d8d05d91735ec9d936354f27778261dc5
CLARK JERIA FRIAS <CLARK JERIA FRIAS>
2016-11-29 15:53:22 -0800

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.scss'],
+  templateUrl: 'app.template.html'
+})
+
+export class USGSApp {
+
+  constructor() {}
+
+}
---
+++ src/pages/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import { AppState } from '../../services/app.service';
+import { Title } from './title';
+import { XLarge } from './x-large';
+
+@Component({
+  // The selector is what angular internally uses
+  // for `document.querySelectorAll(selector)` in our index.html
+  // where, in this case, selector is the string 'home'
+  selector: 'home',  // <home></home>
+  // We need to tell Angular's Dependency Injection which providers are in our app.
+  providers: [
+    Title
+  ],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+  // Set our default values
+  localState = { value: '' };
+  // TypeScript public modifiers
+  constructor(public appState: AppState, public title: Title) {
+
+  }
+
+  ngOnInit() {
+    console.log('hello `Home` component');
+    // this.title.getData().subscribe(data => this.data = data);
+  }
+
+  submitState(value: string) {
+    console.log('submitState', value);
+    this.appState.set('value', value);
+    this.localState.value = '';
+  }
+}
