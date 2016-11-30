7bbaf6ca3a6913cddd34f8c5c2fbaa54f8f668e8
CLARK JERIA FRIAS <CLARK JERIA FRIAS>
2016-11-30 10:16:37 -0800

    prod api ip

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -6,5 +6,10 @@ export class Configuration {
     public ApiUrl: string = "api/";
     public ServerWithApiUrl = this.Server + this.ApiUrl;
 
-    constructor() { }
+    constructor() {        
+        if ('production' === ENV) {
+                this.Server = "http://10.214.2.47:8080/";
+                this.ServerWithApiUrl = this.Server + this.ApiUrl;
+        }
+     }
 }
